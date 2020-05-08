416b638ac3bc1cbb047bd627a9fcdb5966800f6a
aishwarya105 <aishwarya105@gmail.com>
2020-05-08 15:25:32 -0700

    Add files via upload

--- Flask-Project/db/init.sql
+++ Flask-Project/db/init.sql
@@ -1,1 +1,15 @@
+CREATE DATABASE yolo_frames;
+use yolo_frames;
 
+CREATE TABLE frames (
+    Id INT PRIMARY KEY AUTO_INCREMENT,
+    FrameNum INT,
+    x INT,
+    y INT,
+    w INT,
+    h INT,
+    t TIMESTAMP,
+    hr INT,
+    min INT,
+    sec INT
+);
